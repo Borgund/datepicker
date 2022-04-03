385647f33a32bee0e8fe050d0cc215fa0147057e
Adrianbo <akb-92@hotmail.com>
2022-04-03 20:02:12 +0200

    add timeslot component

--- src/components/TimeSlots/TimeSlots.tsx
+++ src/components/TimeSlots/TimeSlots.tsx
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { TimeToggle } from "..";
 import { Slot } from "../../types";
+import { DateContext } from "../DatePicker";
+import styles from "./TimeSlots.module.scss";
 
 type Props = { slots: Slot[] };
 
 export const TimeSlots = ({ slots }: Props) => {
-  const [selectedSlot, setSelectedSlot] = useState<Slot>();
+  const { selectedSlot, setSelectedSlot } = useContext(DateContext);
   return (
-    <>
+    <div className={styles.slotWrapper}>
       {slots.map((slot) => (
         <TimeToggle
           slot={slot}
           key={slot.time}
-          on_click={(slot: Slot) => setSelectedSlot(slot)}
+          on_click={(slot: Slot) => setSelectedSlot?.(slot)}
           active={selectedSlot?.time === slot.time}
         />
       ))}
-    </>
+    </div>
   );
 };
